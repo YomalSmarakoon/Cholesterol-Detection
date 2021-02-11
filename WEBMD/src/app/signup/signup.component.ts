4bb04549fadd7ba3da91a52217cadf9d80eb8ec9
Chanuth <50017558+Chanuth@users.noreply.github.com>
2021-02-11 14:59:56 +0530

    added validation part to the signin page

--- WEBMD/src/app/signup/signup.component.ts
+++ WEBMD/src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { passValidator } from './validator';
 
 @Component({
   selector: 'app-signup',
@@ -7,6 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent {
   hide = true;
+  form: FormGroup;
   
-  constructor() {}
-    }
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.form  = this.fb.group({
+      Name: ['', [Validators.required]],
+      Lastname: ['', [Validators.required]],
+      Email:['',[Validators.required, Validators.email]],
+      Password:['',[Validators.required, Validators.minLength(6)]],
+      confirmPassword:['', passValidator]
+   });
+  }
+
+  onSubmit(){
+    if (this.form.valid) {
+      this.router.navigate(['user-detail-form']);
+}else{
+  Validators.required;
+}
+}
+}
